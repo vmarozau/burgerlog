c511e5505e96046ac6fa91104dcdcc8235201e50
vmarozau <vyacheslav.morozov1@gmail.com>
2019-01-20 22:17:09 +0300

    First commit

---
+++ burgerlog/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import burger from './burger.jpg';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Burgerlog is going to be awesome.
+          </p>
+          <img src={burger} className="Burger-pic" alt="nice picture of my burger" />
+
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
