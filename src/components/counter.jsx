15c20ecc7df65a907e08b3cabfb6b79690947356
Viachaslau Marozau <vyacheslav.morozov1@gmail.com>
2019-01-30 00:42:55 +0300

    Add Menu

---
+++ src/components/counter.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
+  render() {
+    return (
+      <div>
+        <React.Fragment>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button style={{ fontSize: 50 }} className="btn btn-secondary btn-sm">
+            Increment
+          </button>
+
+          <ul>
+            {this.state.tags.map(tag => (
+              <li key={tag}>{tag} </li>
+            ))}
+          </ul>
+        </React.Fragment>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+export default Counter;
--- src/components/menuTest.jsx
+++ src/components/menuTest.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import burger from "./burger.jpg";
 import { scaleDown as Menu } from "react-burger-menu";
 
-class App extends Component {
+class MenuTest extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
+  };
   styles2 = {
     bmBurgerButton: {
       position: "fixed",
@@ -56,47 +57,23 @@ class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <Menu styles={this.styles2}>
           <a id="home" className="menu-item" href="/">
             Home
           </a>
-          <br />
           <a id="about" className="menu-item" href="/about">
             About
           </a>
-          <br />
           <a id="contact" className="menu-item" href="/contact">
             Contact
           </a>
-          <br />
           <a onClick={this.showSettings} className="menu-item--small" href="">
             Settings
           </a>
         </Menu>
-
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>This burgerlog app is going to be awesome.</p>
-            <img
-              src={burger}
-              className="Burger-pic"
-              alt="nice picture of my burger"
-            />
-            {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-          </header>
-        </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
-
-export default App;
+export default MenuTest;
